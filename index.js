c4791fcb6ee4e303df3f4f6c1629c9437a2f66b6
SAM EINSIDLER <sameinsidler@SAMs-Air.attlocal.net>
2020-08-20 21:10:56 -0500

    initial push

---
+++ index.js
@@ -0,0 +1,13 @@
+"use strict";
+
+const searchUrl = "hhtps://www.github.com/users/{username}/repos";
+
+function formatQueryParams(params) {}
+
+function getRepos(username, maxResults = 10) {
+  const params = {
+    type: owner,
+    sort: full_name,
+    direction: asc,
+  };
+}
