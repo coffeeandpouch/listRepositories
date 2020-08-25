e43f805341ffc6c78648a0615df7abe88f77cf7f
SAM EINSIDLER <sameinsidler@SAMs-Air.attlocal.net>
2020-08-25 10:58:22 -0500

    latest changes

--- index.js
+++ index.js
@@ -1,68 +1,66 @@
 "use strict";
 
-const searchUrl = `'hhtps://www.github.com/users/${username}/repos'`;
-
-
 function formatQueryParams(params) {
-  const queryItems = Object.keys(params)
-  .map(key => `${encodeURIComponent(key)}=$
-  {encodeURIComponent(params[key])}`)
-  return queryItems.join('&');
+  const queryItems = Object.keys(params).map(
+    (key) => `${encodeURIComponent(key)}=${encodeURIComponent(params[key])}`
+  );
+  return queryItems.join("&");
 }
 
-function displayResults(responseJson, per_page)
-{
+function displayResults(responseJson, per_page) {
   //if there are previous results, remove them
   console.log(responseJson);
-  $('#results-list').empty();
+  $("#results-list").empty();
   //iterate through the repositories array stopping at the max number of results
-  for (let i = 0; i < responseJson.value.length & i<per_page ; i++){
-    $('#results-list').append(
+  for (let i = 0; (i < responseJson.value.length) & (i < per_page); i++) {
+    $("#results-list").append(
       `<li><h3><a href="$(responseJson.value[i].url}">$(responseJson.value[i].title}</a></h3>
         </li>`
-        )};
+    );
+  }
   //display the results section
-  $('#results-list').removeClass('hidden');
-};
-
-function getRepos(username, type, per_page) {
-  const params = {
-    // accept = "application/vnd.github.v3+json",
-    username = "#js-search-term",
-    type = "all", 
-    per_page = 10
-  };
-  const queryString = formatQueryParams(params)
-  const url = searchUrl + '?' + queryString;
-
-  console.log(url);
+  $("#results-list").removeClass("hidden");
 }
 
-const options = {
-  headers = new headers ({
-    accept:"application/vnd.github.v3+json"})
-};
+function getRepos(username,type, per_page) {
+  // username = $("#js-search-term").val()
+  const searchUrl = `https://www.github.com/users/${username}/repos`;
+  const params = {
+    // accept = "application/vnd.github.v3+json",
 
-fetch(url, options)  
-  .then(response => {
-    if (response.ok) {
-      return response.json();
-    }
-    throw new Error(response.statusText);
-  })
-  .then(responseJson => displayResults
-    (responseJson, per_page))
-  .catch(err => {
-    $('#js-error-message').text(`Something went wrong: ${err.message}`);
-  });
-// }
+    type: "all",
+    per_page: 10,
+  };
+  const queryString = formatQueryParams(params);
+  const url = searchUrl + "?" + queryString;
+
+  console.log(url);
+
+  const options = {
+    headers: new Headers({
+      accept: "application/vnd.github.v3+json",
+    }),
+  };
+
+  fetch(url, options)
+    .then((response) => {
+      if (response.ok) {
+        return response.json();
+      }
+      throw new Error(response.statusText);
+    })
+    .then((responseJson) => displayResults(responseJson, per_page))
+    .catch((err) => {
+      $("#js-error-message").text(`Something went wrong: ${err.message}`);
+    });
+}
 
 function watchForm() {
-  $('form').submit(event => {
+  $("form").submit((event) => {
     eventPreventDefault();
-    var username = $('#js-search-term').val();
-    const per_page = $('#js-max-results').val();
-    getRepositiories(username, per_page);
+    var username = $("#js-search-term").val();
+    const per_page = $("#js-max-results").val();
+    getRepos(username, "all", per_page);
   });
 }
 
